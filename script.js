8f94b7d544b079ab0e4008fba860ea46a6fef85a
danmac121 <danmacdonald121@gmail.com>
2023-07-27 14:40:20 -0400

    added an alert for if user does not select any character types. Fixed issue where my math.random was only running once, it was globally scoped but needed to be locally scoped

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 let numbers = [0,1,2,3,4,5,6,7,8,9]
 let special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '-', '[', ']', '.', '/', ',', ';', '<', '>', '?', ':', '|', '+', '_', '`', '~']
-// let lowercaseindex = Math.floor(Math.random()*26);
+
 let builtarray = []
-let generatedLetterIndex = Math.floor(Math.random()*lowercase.length);
-// generate num between 0-10 with no decimal
-// Math.floor(Math.random()*10)
+
+
 let confirmUpper = (confirm("Do you want your password to contain uppercase letters?"))
 let confirmLower = (confirm("Do you want your password to contain lowercase letters?"))
 let confirmNumber = (confirm("Do you want your password to contain numbers?"))
@@ -21,11 +20,12 @@ function generatePassword(){
   
 if (confirmUpper){
   builtarray = [...builtarray, ...uppercase]
-console.log(builtarray)
+  console.log(builtarray);
+
 }
 if (confirmLower){
   builtarray = [...builtarray, ...lowercase]
- console.log(builtarray);
+  console.log(builtarray);
 }
 
 if (confirmNumber){
@@ -36,23 +36,28 @@ if (confirmNumber){
 if (confirmSpecial){
   builtarray = [...builtarray, ...special]
   console.log(builtarray);
-}
 
 }
+if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial){
+alert("You must choose at least one character type to be included in your password.")
 
-generatePassword()
+}
 
-let passwordlength = prompt("Please select a password lenght between 8 and 128 characters.")
-for (let i = 0; i < passwordlength; i++) {
+let password = "";
+let passwordLength = prompt("Please select a password length between 8 and 128 characters.");
+for (let i = 0; i < passwordLength; i++) {
   
+  let generatedLetterIndex = Math.floor(Math.random() * builtarray.length);
+  password += builtarray[generatedLetterIndex];
+ } 
+  return password;
   
-  password += "x";
 
-//   return "x"
+
+
+
+
 }
-
-
-
 
 
 
